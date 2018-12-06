8bd6679e68d87b349a05b32358abdb8b6b3f3b57
Max Bondarenko <hrabizic@gmail.com>
2018-12-06 20:25:53 +0300

    Fix errors in web project

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const logo = require("./logo.svg") as string;
 
 class App extends Component {
   render() {
