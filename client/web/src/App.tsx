7738001ffa11d52a258569c232d7d4488a1e53c0
Max Bondarenko <hrabizic@gmail.com>
2018-12-08 17:40:17 +0300

    Install prettier and tslint

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const logo = require("./logo.svg") as string;
+const logo = require('./logo.svg') as string;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export class App extends Component {
+	public render(): JSX.Element {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+					<a
+						className='App-link'
+						href='https://reactjs.org'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
-
-export default App;
