1b1502f9bc6f8432dce3014f4a2ac9231c6c3435
Max Bondarenko <hrabizic@gmail.com>
2018-12-09 17:35:35 +0300

    Update swagger path: swagger -> api/docs

--- server/Tempocracy.API/Startup.cs
+++ server/Tempocracy.API/Startup.cs
@@ -19,15 +19,13 @@ namespace Tempocracy.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
             services.AddRouting(options => options.LowercaseUrls = true);
             services.Configure<ConnectionOptions>(Configuration);
-
-            // Register the Swagger generator, defining 1 or more Swagger documents
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "Tempocracy API", Version = "v1" });
@@ -43,19 +41,14 @@ namespace Tempocracy.API
             builder.RegisterModule<DomainModule>();
             builder.RegisterModule<InfrastructureModule>();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
-            // specifying the Swagger JSON endpoint.
+            app.UseSwagger(c => { c.RouteTemplate = "api/docs/{documentName}/swagger.json"; });
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tempocracy API V1");
-                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/api/docs/v1/swagger.json", "Tempocracy API V1");
+                c.RoutePrefix = "api/docs";
             });
 
             if (env.IsDevelopment())
@@ -67,7 +60,7 @@ namespace Tempocracy.API
                 app.UseHsts();
             }
 
-            // Shows UseCors with CorsPolicyBuilder.
+            // Allow requests from local Front-end project.
             app.UseCors(builder =>
                 builder.WithOrigins("http://localhost:3000"));
 
